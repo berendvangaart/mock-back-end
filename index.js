ad0098367f9b7397cc8f979e8b591e674302d02f
Berend van Gaart <berendvangaart@gmail.com>
2022-12-06 10:08:22 +0100

    make get all, post and delete endpoint

--- index.js
+++ index.js
@@ -6,5 +6,24 @@ const port = 8080;
 app.use(cors())
 app.use(express.json());
 
+let data = []
+
+app.get('/', (req, res) => {
+    console.log("call GET/")
+    res.send(data);
+})
+
+app.post('/', (req, res) => {
+    console.log("Body: ", req.body)
+    data.push(req.body)
+    res.send(data);
+})
+
+app.delete('/', (req, res) => {
+    console.log("Body: ", req.body)
+    data = []
+    res.send(data);
+})
+
 
 app.listen(port, () => console.log(`🚀🚀 server UP: Listening on port ${port} 🚀🚀`))
