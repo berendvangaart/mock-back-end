9d3477a33863dfb35e81afffa99f0de6aa8955db
Berend van Gaart <berendvangaart@gmail.com>
2023-01-19 10:13:21 +0100

    update delete request

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * This is a mock backend for debugging purposes
+ * */
+
 const express = require('express');
 const cors = require("cors");
 const app = express();
@@ -8,23 +12,37 @@ app.use(express.json());
 
 let data = []
 
+/**
+ * return all elements
+ */
 app.get('/', (req, res) => {
     console.log("call GET/")
     res.send(data);
 })
 
+/**
+ * add new element to data
+ */
 app.post('/', (req, res) => {
     console.log("Body: ", req.body)
     data.push(req.body)
     res.send(data);
 })
 
-app.delete('/', (req, res) => {
-    console.log("Body: ", req.body)
-    data = []
+/**
+ * remove element with coresponding id from data
+ */
+app.delete('/:id', (req, res) => {
+    console.log("ID: ", req.params.id)
+    data = data.filter(element => {
+        return element.id !== req.params.id
+    })
     res.send(data);
 })
 
+/**
+ * update element by id
+ */
 app.put('/', (req, res) => {
     console.log("Body: ", req.body)
 
